b49fe59d7e66c94c0f36f7f592a8d9c36a53a4f6
Vladimir Brasil <vladimir.brasil@gmail.com>
2013-02-09 21:01:36 -0200

    Inicio

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Claropj::Application.config.secret_token = '3e91868197324bf7ff258cf8245f67bfd2a6e5d091031a6d740849577b7a41b57086b969009a29102b902debbcde6950c3b6e28ae61fb0ab10b2fb768c21dc94'
